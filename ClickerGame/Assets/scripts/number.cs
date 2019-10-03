603cb1b1224265829880864dfeba510fcd59d968
RyanBroadway <broadwayryan123@gmail.com>
2019-10-03 16:23:13 +0100

    V1.0.0 - Added button - Added number - Added background - Added button script

---
+++ ClickerGame/Assets/scripts/number.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class number : MonoBehaviour
+{
+    int click = 0;
+
+    public void Start()
+    {
+
+    }
+
+    public void Onclick()
+    {
+        click = click + 1;
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = click.ToString();
+    }
+}
